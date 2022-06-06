340324945d1310f5bee770419b257704b2ff9048
hihiroo <alsrud606@hanyang.ac.kr>
2022-06-06 06:34:17 +0000

    fix: fix drawing bbox top line

--- sfa/utils/visualization_utils.py
+++ sfa/utils/visualization_utils.py
@@ -127,10 +127,10 @@ def draw_box_2d(image, corners, color=(0, 0, 255)):
         max_x = max(max_x, corners[i][0])
         max_y = max(max_y, corners[i][1])
     
-    cv2.line(image, (min_x, min_y), (min_x, max_y), color, 1, lineType=cv2.LINE_AA)
-    cv2.line(image, (min_x, max_y), (max_x, max_y), color, 1, lineType=cv2.LINE_AA)
-    cv2.line(image, (max_x, max_y), (max_x, min_y), color, 1, lineType=cv2.LINE_AA)
-    cv2.line(image, (min_x, max_y), (min_x, min_y), color, 1, lineType=cv2.LINE_AA)
+    cv2.line(image, (min_x, min_y), (min_x, max_y), color, 2, lineType=cv2.LINE_AA)
+    cv2.line(image, (min_x, max_y), (max_x, max_y), color, 2, lineType=cv2.LINE_AA)
+    cv2.line(image, (max_x, max_y), (max_x, min_y), color, 2, lineType=cv2.LINE_AA)
+    cv2.line(image, (max_x, min_y), (min_x, min_y), color, 2, lineType=cv2.LINE_AA)
     
     return image
 
